db8848367e08e84aff19f0a153cb8794f103f2b9
Iain Sproat <68657+iainsproat@users.noreply.github.com>
2022-11-13 00:35:40 +0000

    Only set header when finished publishing all alerts

--- pkg/alertforwarder/alertforwarder.go
+++ pkg/alertforwarder/alertforwarder.go
@@ -6,7 +6,6 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/specklesystems/alertmanager-discord/pkg/alertmanager"
 	"github.com/specklesystems/alertmanager-discord/pkg/discord"
@@ -40,58 +39,31 @@ func (af *AlertForwarder) sendWebhook(amo *alertmanager.Out, w http.ResponseWrit
 		groupedAlerts[alert.Status] = append(groupedAlerts[alert.Status], alert)
 	}
 
+	failedToPublishAny := false
+
 	for status, alerts := range groupedAlerts {
-		DO := discord.Out{}
-
-		RichEmbed := discord.Embed{
-			Title:       fmt.Sprintf("[%s:%d] %s", strings.ToUpper(status), len(alerts), amo.CommonLabels.Alertname),
-			Description: amo.CommonAnnotations.Summary,
-			Color:       discord.ColorGrey,
-			Fields:      []discord.EmbedField{},
-		}
-
-		switch status {
-		case alertmanager.StatusFiring:
-			RichEmbed.Color = discord.ColorRed
-		case alertmanager.StatusResolved:
-			RichEmbed.Color = discord.ColorGreen
-		}
-
-		if amo.CommonAnnotations.Summary != "" {
-			DO.Content = fmt.Sprintf(" === %s === \n", amo.CommonAnnotations.Summary)
-		}
-
-		for _, alert := range alerts {
-			realname := alert.Labels["instance"]
-			if strings.Contains(realname, "localhost") && alert.Labels["exported_instance"] != "" {
-				realname = alert.Labels["exported_instance"]
-			}
-
-			RichEmbed.Fields = append(RichEmbed.Fields, discord.EmbedField{
-				Name:  fmt.Sprintf("[%s]: %s on %s", strings.ToUpper(status), alert.Labels["alertname"], realname),
-				Value: alert.Annotations.Description,
-			})
-		}
-
-		DO.Embeds = []discord.Embed{RichEmbed}
+		DO := TranslateAlertManagerToDiscord(status, amo, alerts)
 
 		res, err := af.client.PublishMessage(DO)
 		if err != nil {
 			err = fmt.Errorf("Error encountered when publishing message to discord: %w", err)
 			log.Printf("%s", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			failedToPublishAny = true
+			continue
 		}
 
 		if res.StatusCode < 200 || res.StatusCode > 399 {
-			w.WriteHeader(http.StatusInternalServerError)
-			if res.Body != nil {
-				io.Copy(w, res.Body)
-			}
+			failedToPublishAny = true
+			continue
 		}
-
-		w.WriteHeader(http.StatusOK)
 	}
+
+	if failedToPublishAny {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func (af *AlertForwarder) sendRawPromAlertWarn() (*http.Response, error) {
